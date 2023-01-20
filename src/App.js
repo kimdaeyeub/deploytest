51831cabf975dad43dab7fc86af74cd3e5e9337a
kimdaeyeub <kdy3915@naver.com>
2023-01-20 09:22:32 +0900

    #1.0 init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onPlus = () => {
+    setCount((prev) => prev + 1);
+  };
+  const onMinus = () => {
+    setCount((prev) => prev - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Count</h1>
+      <h1>{count}</h1>
+      <button onClick={onPlus}>+</button>
+      <button onClick={onMinus}>-</button>
     </div>
   );
 }
