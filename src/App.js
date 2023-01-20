1ffd1302529718388438c33e36a73a2e8bac8bb3
kimdaeyeub <kdy3915@naver.com>
2023-01-20 10:19:45 +0900

    #2.0 router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import { useState } from "react";
+import AppRouter from "./components/Router";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const onPlus = () => {
-    setCount((prev) => prev + 1);
-  };
-  const onMinus = () => {
-    setCount((prev) => prev - 1);
-  };
-  return (
-    <div>
-      <h1>Count</h1>
-      <h1>{count}</h1>
-      <button onClick={onPlus}>+</button>
-      <button onClick={onMinus}>-</button>
-    </div>
-  );
+  return <AppRouter />;
 }
 
 export default App;
